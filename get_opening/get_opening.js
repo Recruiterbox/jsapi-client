61d319f03984d5bcaa240b0cdff4483e28558906
Vinod <vinod@Nivedhas-MacBook-Pro-2.local>
2016-02-12 11:20:51 +0530

    Corrected spelling mistake

--- get_opening/get_opening.js
+++ get_opening/get_opening.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   //Sample JQuery code
   $.ajax({
-    url: 'https://jspai.recruiterbox.com/v1/openings/fk0hmzg?client_name=demoaccount',
+    url: 'https://jsapi.recruiterbox.com/v1/openings/fk0hmzg?client_name=demoaccount',
     success: function(response) {
       $("#json-response").html(JSON.stringify(response));
     }
